7a34e5c0d1b7b865a38e9ba6c0d62f7b3fa0f7ca
ForeytKL22 <krforeyt@gmail.com>
2021-11-07 15:39:23 -0700

    added auth.js and added authorization to routes

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     // console.log(req.session);
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Donec posuere metus vitae ipsum.',
-    post_body: ';asodfaoshidfad',
+    post_body: 'Lorem ipsum dolor sit amet, consectetu ',
     user_id: 9
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_body: ';asodfaoshidfad',
+    post_body: 'tore veritatis et quasi architecto beatae enim ipsam voluptatem quiar aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, ',
     user_id: 1
   },
   {
     title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_body: ';asodfaoshidfad',
+    post_body: 'epturi sint occaecati cupiditate non providtia animi, id est laborum et dolorum fuga. Et harum qu',
     user_id: 6
   },
   {
@@ -23,7 +23,7 @@ const postdata = [
   },
   {
     title: 'Pellentesque eget nunc.',
-    post_body: ';asodfaoshidfad',
+    post_body: 'oribus autem quibu aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. ',
     user_id: 2
   },
   {
@@ -33,7 +33,7 @@ const postdata = [
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_body: ';asodfaoshidfad',
+    post_body: 'oribus autem quiut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. ',
     user_id: 3
   }
 ];
