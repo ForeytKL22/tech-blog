0515524a634a9578343b43187016411c20f19c53
ForeytKL22 <krforeyt@gmail.com>
2021-11-07 16:26:05 -0700

    trying to get edit post page to render

--- controllers/dashboard-route.js
+++ controllers/dashboard-route.js
@@ -60,7 +60,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
             if (dbPostData) {
                 const post = dbPostData.get({ plain: true });
                 res.render('edit-post', {
-                    post,
+                    post, 
                     loggedIn: true
                 });
             } else {
