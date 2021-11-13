fb2bc7ab1844a8d0fc9e21f8801261cd94c9b8bb
ForeytKL22 <krforeyt@gmail.com>
2021-11-13 10:13:03 -0700

    completed app

--- db/schema.sql
+++ db/schema.sql
@@ -3,16 +3,3 @@ DROP DATABASE IF EXISTS tech_blog;
 CREATE DATABASE tech_blog;
 
 
-
--- make sure server is connecting to database first and that we can create data in insomnia, then work on routes, then the front end part
-
-
-
-
--- comment table: id, body, createdAt, updatedAt, postId, userId
-
--- posts table: id, title, body, createdAt, updatedAt, userId
-
--- user table: id, username, password(hashed pw)
-
--- session table: sid(auto generated), expires, data(auto generated, cookies?), createdAt, updatedAt (worry about this one last)
